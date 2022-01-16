db4f60460be8a21fe8426bf92a44b76a31debaf7
Raed Elsayed <Roy.Elsayed@nhsbt.nhs.uk>
2022-01-16 12:54:33 +0000

    remove and update

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactCard from './ContactCard';
+import ContactList from './ContactList';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>Hello React world</div>
+    <ContactCard/>
+    </>
+    
+
+  );
+}
+
+export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ContactCard = () => <div>I am in</div>
+
+export default ContactCard;
